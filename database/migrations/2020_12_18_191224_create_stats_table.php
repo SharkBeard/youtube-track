1b77948fab99f5794b42c4ec59a4a4a6a648b4fa
Aaron Campbell <sharkbeardio@gmail.com>
2020-12-18 15:20:52 -0500

    generate channel + stats

---
+++ database/migrations/2020_12_18_191224_create_stats_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stats', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('channel_id');
+            $table->date('capture_date');
+            $table->integer('view_count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stats');
+    }
+}
