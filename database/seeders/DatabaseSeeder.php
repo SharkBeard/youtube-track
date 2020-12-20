249258810fd9e15afb424f16bf444420553e936d
Aaron Campbell <sharkbeardio@gmail.com>
2020-12-20 03:27:35 -0500

    update seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        \App\Models\Channel::create(['url' => 'http://youtube.com/sips/about']);
+        \App\Models\Channel::create(['url' => 'YogscastSips']);
         for($day = 1; $day <= 15; $day++) {
           $date = \DateTime::createFromFormat('m/d/Y', "12/$day/2020");
           \App\Models\Stats::create(['view_count' => 123456 + rand(-500, 2000), 'capture_date' => $date, 'channel_id' => 1]);
