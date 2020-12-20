018f335453a34dde00d9215eee1e1c7e7be5b735
Aaron Campbell <sharkbeardio@gmail.com>
2020-12-20 07:01:46 -0500

    update to use youtube url

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Channel;
 use App\Models\Stats;
+use App\Operations\StatsUpdateOperation;
 use Illuminate\Http\Request;
 
 class StatsController extends Controller
 {
   public function show($id)
   {
-    $channel = Channel::findOrFail($id);
+    $channel = Channel::where('url', $id)->firstOrFail();
     return view('stats.show', [
       'channel' => $channel,
       'stats' => $channel->stats()->get()
